c461aa185fc9b3553ea2b8dbdbdd68eced85fcf8
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-12 14:55:34 +0200

    Update express.d.ts

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,1 +1,14 @@
+import { User, UserRole } from '../models/User';
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        userId: string;
+        email: string;
+        role: UserRole;
+        locations: string[];
+      };
+    }
+  }
+}
