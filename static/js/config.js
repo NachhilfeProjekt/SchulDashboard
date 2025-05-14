2d19a564dc0e318840fdae0aa69e5cad7825333c
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 18:04:34 +0200

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,7 +1,6 @@
 // static/js/config.js
 const config = {
-  // Verwende einen CORS-Proxy für die API-Calls
-  apiUrl: 'https://cors-anywhere.herokuapp.com/https://schuldashboard-backend.onrender.com/api',
+  apiUrl: 'https://schuldashboard-backend.onrender.com/api',
   tokenKey: 'schul_dashboard_token',
   userKey: 'schul_dashboard_user',
   locationsKey: 'schul_dashboard_locations',
