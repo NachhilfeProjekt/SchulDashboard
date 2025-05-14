b981c1d95de0d92023fc8a05289770acfd648060
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 16:58:19 +0200

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,21 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  root: 'SchulDashboard/frontend/', // Gib das Root-Verzeichnis explizit an
   build: {
     outDir: 'dist',
     sourcemap: false,
     minify: 'terser',
     cssCodeSplit: true
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   },
   server: {
     port: 3000,
