ce8072c4b9052d98a296e4a5581f01dc94a3fe76
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 17:25:45 +0200

    Create config.js

---
+++ static/js/config.js
@@ -0,0 +1,8 @@
+// Konfiguration für die Anwendung
+const config = {
+  apiUrl: 'https://schuldashboard-backend.onrender.com/api',
+  tokenKey: 'schul_dashboard_token',
+  userKey: 'schul_dashboard_user',
+  locationsKey: 'schul_dashboard_locations',
+  currentLocationKey: 'schul_dashboard_current_location'
+};
