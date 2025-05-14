35d9021281eb06741099e8fea5449f96d6170469
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 22:43:53 +0200

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+// frontend/src/components/Layout.tsx
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { RootState } from '../store/store';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import { Box, CssBaseline, Toolbar } from '@mui/material';
+import { Box, CssBaseline, Toolbar, Typography } from '@mui/material';
 
 const Layout: React.FC = () => {
   const { user } = useSelector((state: RootState) => state.auth);
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log('Aktuelle Route:', location.pathname);
+  }, [location]);
 
   return (
     <Box sx={{ display: 'flex' }}>
@@ -16,6 +22,9 @@ const Layout: React.FC = () => {
       {user && <Sidebar />}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         {user && <Toolbar />} {/* Spacing to push content below AppBar */}
+        <Typography variant="caption" display="block" gutterBottom>
+          Aktuelle Route: {location.pathname}
+        </Typography>
         <Outlet /> {/* This allows for nested routes to render here */}
       </Box>
     </Box>
