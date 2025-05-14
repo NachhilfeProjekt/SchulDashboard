0269bf5fad463582ca8c83ff4898da3140cd4f90
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-15 00:35:25 +0200

    Update Layout.tsx

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,15 +1,28 @@
 // frontend/src/components/Layout.tsx
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
-import { Outlet, useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState, AppDispatch } from '../store/store';
+import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import { Box, CssBaseline, Toolbar, Typography } from '@mui/material';
+import { Box, CssBaseline, Toolbar } from '@mui/material';
+import { initializeApp } from '../utils/initializeApp';
 
 const Layout = () => {
-  const { user } = useSelector((state: RootState) => state.auth);
+  const { user, token } = useSelector((state: RootState) => state.auth);
   const location = useLocation();
+  const dispatch = useDispatch<AppDispatch>();
+  const navigate = useNavigate();
+
+  // Überprüfen, ob der Benutzer authentifiziert ist
+  useEffect(() => {
+    if (!token) {
+      navigate('/login');
+    } else if (!user) {
+      // Benutzerinformationen laden, wenn Token vorhanden aber User nicht
+      initializeApp(dispatch);
+    }
+  }, [token, user, dispatch, navigate]);
 
   console.log('Layout gerendert. Aktueller Pfad:', location.pathname);
   console.log('User:', user);
@@ -20,8 +33,8 @@ const Layout = () => {
       {user && <Header />}
       {user && <Sidebar />}
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        {user && <Toolbar />} {/* Spacing to push content below AppBar */}
-        <Outlet /> {/* This allows for nested routes to render here */}
+        {user && <Toolbar />} {/* Spacing für Abstand unter Header */}
+        <Outlet /> {/* Hier werden die verschachtelten Routen gerendert */}
       </Box>
     </Box>
   );
