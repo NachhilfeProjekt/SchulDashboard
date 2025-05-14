6ad47520bb2297ccaa7e644bef08b5d05dfcfec2
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-15 00:37:12 +0200

    Delete static directory

--- static/js/config.js
+++
@@ -1,8 +0,0 @@
-// In static/js/config.js
-const config = {
-  apiUrl: 'https://dashboard-backend-uweg.onrender.com/api', // Einheitliche API-URL
-  tokenKey: 'schul_dashboard_token',
-  userKey: 'schul_dashboard_user',
-  locationsKey: 'schul_dashboard_locations',
-  currentLocationKey: 'schul_dashboard_current_location'
-};
