a918bf3e77b8e74e2dfaf8dbb3e3dcc96190f0a1
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 16:21:18 +0200

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,53 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from './store/store';
 import Layout from './components/Layout';
 import LoginPage from './pages/LoginPage';
 import DashboardPage from './pages/DashboardPage';
 import EmailPage from './pages/EmailPage';
 import ManageUsersPage from './pages/ManageUsersPage';
 import ManageButtonsPage from './pages/ManageButtonsPage';
+import SettingsPage from './pages/SettingsPage';
+import AdminPage from './pages/AdminPage';
+import CreateLeadPage from './pages/CreateLeadPage';
 import PrivateRoute from './components/PrivateRoute';
 import ResetPasswordPage from './pages/ResetPasswordPage';
+import { initializeApp } from './utils/initializeApp';
+
+// AppInitializer Komponente zum Initialisieren des App-Status
+const AppInitializer: React.FC = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  
+  useEffect(() => {
+    initializeApp(dispatch);
+  }, [dispatch]);
+  
+  return <Outlet />;
+};
 
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <Routes>
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          
-          <Route path="/" element={<PrivateRoute><Layout /></PrivateRoute>}>
-            <Route index element={<DashboardPage />} />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        
+        <Route element={<AppInitializer />}>
+          <Route element={<PrivateRoute><Layout /></PrivateRoute>}>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/dashboard" element={<DashboardPage />} />
             <Route path="/email" element={<EmailPage />} />
             <Route path="/manage-users" element={<ManageUsersPage />} />
             <Route path="/manage-buttons" element={<ManageButtonsPage />} />
+            <Route path="/settings" element={<SettingsPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route path="/create-lead" element={<CreateLeadPage />} />
+            <Route path="*" element={<Navigate to="/dashboard" replace />} />
           </Route>
-        </Routes>
-      </Router>
-    </Provider>
+        </Route>
+      </Routes>
+    </Router>
   );
 };
 
