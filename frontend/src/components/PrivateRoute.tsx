ee2f427d02ef4e66597aff8da5270aee1e68fe43
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 22:49:08 +0200

    Update PrivateRoute.tsx

--- frontend/src/components/PrivateRoute.tsx
+++ frontend/src/components/PrivateRoute.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
+// frontend/src/components/PrivateRoute.tsx
+import React, { useEffect } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { RootState } from '../store/store';
 
@@ -9,11 +10,19 @@ interface PrivateRouteProps {
 
 const PrivateRoute: React.FC<PrivateRouteProps> = ({ children }) => {
   const { token } = useSelector((state: RootState) => state.auth);
+  const location = useLocation();
+  
+  useEffect(() => {
+    console.log("PrivateRoute wird gerendert für Pfad:", location.pathname);
+    console.log("Authentifizierungsstatus:", token ? "Authentifiziert" : "Nicht authentifiziert");
+  }, [location.pathname, token]);
 
   if (!token) {
+    console.log("Keine Authentifizierung - Umleitung zur Login-Seite");
     return <Navigate to="/login" replace />;
   }
 
+  console.log("Authentifiziert - Rendering der geschützten Route");
   return <>{children}</>;
 };
 
