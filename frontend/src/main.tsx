818708b4b6466c813076cce018949af1f41d8393
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 16:21:02 +0200

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { store } from './store/store';
 import { Provider } from 'react-redux';
+import store from './store/store';
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 import reportWebVitals from './reportWebVitals';
 
