82d31c3bae10bbbf3635588a7c92f95968eb8b8b
NachhilfeProjekt <luca.duesdieker@gmail.com>
2025-05-14 21:41:30 +0200

    Update config.js

--- static/js/config.js
+++ static/js/config.js
@@ -1,6 +1,6 @@
 // static/js/config.js
 const config = {
-  apiUrl: 'https://schuldashboard-backend.onrender.com/api',
+  apiUrl: 'https://dashboard-backend-uweg.onrender.com/api',
   tokenKey: 'schul_dashboard_token',
   userKey: 'schul_dashboard_user',
   locationsKey: 'schul_dashboard_locations',
